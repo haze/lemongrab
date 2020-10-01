3026e7efe01d0617165540633c6dc16935d581a1
haze <isnt@haze.cool>
2020-10-01 09:55:26 -0400

    Update lemongrab.js
    Move ID check to fast path to avoid slow message checking
    Create constant for bot ID

--- lemongrab.js
+++ lemongrab.js
@@ -1,15 +1,16 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
+const SHEEPY_BOT_ID = '329668530926780426';
+
 client.on("ready", () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
 client.on("message", msg => {
   if (
-    msg.content.includes("to pick it up") &&
-    //sheepy bot
-    msg.member.id === "329668530926780426"
+    msg.member.id === SHEEPY_BOT_ID &&
+    msg.content.includes("to pick it up")
   ) {
     msg.channel.send(">pick");
   }
